c6854022e4fa091d46b49f025cda13a148839d14
Ruslan <Giovingo92@gmail.com>
2024-08-24 22:03:59 +0300

    adding serch value

---
+++ src/components/overscroll-contain.tsx
@@ -0,0 +1,22 @@
+export interface OverscrollContainProps {}
+
+export default function OverscrollContain({}: OverscrollContainProps) {
+  return (
+    <section className='p-10'>
+      <div className='overscroll-contain'>
+        Well, let me tell you something, funny boy. Y&apos;know that little
+        stamp, the one that says &quot;New York Public Library&quot;? Well that
+        may not mean anything to you, but that means a lot to me. One whole hell
+        of a lot. Sure, go ahead, laugh if you want to. I&apos;ve seen your type
+        before: Flashy, making the scene, flaunting convention. Yeah, I know
+        what you&apos;re thinking. What&apos;s this guy making such a big stink
+        about old library books? Well, let me give you a hint, junior. Maybe we
+        can live without libraries, people like you and me. Maybe. Sure,
+        we&apos;re too old to change the world, but what about that kid, sitting
+        down, opening a book, right now, in a branch at the local library and
+        finding drawings of pee-pees and wee-wees on the Cat in the Hat and the
+        Five Chinese Brothers? Doesn&apos;t HE deserve better?
+      </div>
+    </section>
+  );
+}
